ae6ee71429aa6003566f141fa2b08b49b965bc33
hashiqi <lh1357ke@163.com>
2018-04-26 09:30:35 +0800

    second commit

--- src/com/all/play/TwTestOne.java
+++ src/com/all/play/TwTestOne.java
@@ -3,7 +3,14 @@ package com.all.play;
 public class TwTestOne {
 
 	public static void main(String[] args) {
-
+		// TODO Auto-generated method stub
+				System.out.println("this is a test!");
+				System.out.println("this is a test!");
+				System.out.println("this is a test!");
+				System.out.println("this is a test!");
+				System.out.println("this is a test!");
+				System.out.println("this is a test!");
+				System.out.println("this is a test!");
 	}
 
 }
